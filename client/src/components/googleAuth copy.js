80d4fa4aeb608294bbdf2643fd4836d79710e53a
Fabioxr1 <musicfeedinf@gmail.com>
2021-02-15 11:58:53 +0100

    key empty

--- client/src/components/googleAuth copy.js
+++ client/src/components/googleAuth copy.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY_AUTH = '10';
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY_AUTH = '';
 ;
 
 class GoogleAuth extends React.Component {
